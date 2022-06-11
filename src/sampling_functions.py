9bed1f62c3f61c57406f6038b8b41f1326e1c140
Warvito <ianonimato@hotmail.com>
2022-06-11 14:38:32 +0100

    initial

--- src/sampling_functions.py
+++ src/sampling_functions.py
@@ -1,8 +1,26 @@
 import nibabel as nib
+import time
 
 model = None
+image_data = None
 
-def sample():
+def sample_fn(
+        seed,
+        gender_radio,
+        age_slider,
+        ventricular_slider,
+        brain_slider,
+):
+    print(seed)
+    print(gender_radio)
+    print(age_slider)
+    print(ventricular_slider)
+    print(brain_slider)
     image = nib.load("/home/walter/Downloads/sub-HCD0001305_ses-01_space-MNI152NLin2009aSym_T2w.nii.gz")
+    global image_data
     image_data = image.get_fdata()
-    return image_data
+    time.sleep(2)
+
+    plotted_img = image_data[:, :, 80]
+    plotted_img = (plotted_img - plotted_img.min()) / (plotted_img.max() - plotted_img.min())
+    return plotted_img
--- src/start_gradio_app.py
+++ src/start_gradio_app.py
@@ -1,5 +1,5 @@
 import gradio as gr
-from sampling_functions import sample
+from sampling_functions import sample_fn, image_data
 
 title = "Amigo's Brains with Diffusion Models"
 description = """
@@ -38,18 +38,103 @@ inputs_block = [
     ),
 ]
 
-model = None
+def update_plotted_img(idx):
+    global image_data
+    return image_data[:,:,idx]
 
 
-demo = gr.Interface(
-    sample,
-    inputs_block,
-    "number",
-    title=title,
-    description=description,
-    article=article,
-    flagging_dir="/media/walter/Storage/Projects/gradio_medical_ldm/src/flagged"
-)
+demo = gr.Blocks()
 
-demo.launch()
+with demo:
+    gr.Markdown(
+        "<h1 style='text-align: center; margin-bottom: 1rem'>"
+        + title
+        + "</h1>"
+    )
+    gr.Markdown(description)
+    with gr.Box():
+        gr.Markdown("<h2>Inputs<h2>")
+        with gr.Row():
+            seed = gr.Number(
+                value=42,
+                label="Seed",
+                interactive=True,
+            )
+            gender_radio = gr.Radio(
+                choices=["Female", "Male"],
+                value="Female",
+                type="index",
+                label="Gender",
+                interactive=True,
+            )
+            age_slider = gr.Slider(
+                minimum=44,
+                maximum=82,
+                value=63,
+                label="Age [years]",
+                interactive=True,
+            )
+        with gr.Row():
+            ventricular_slider = gr.Slider(
+                minimum=0,
+                maximum=1,
+                value=0.5,
+                label="Volume of ventricular cerebrospinal fluid",
+                interactive=True,
+            )
+            brain_slider = gr.Slider(
+                minimum=0,
+                maximum=1,
+                value=0.5,
+                label="Volume of brain, grey+white matter (normalised for head size)",
+                interactive=True,
+            )
+        with gr.Row():
+            clear_btn = gr.Button("Clear")
+            submit_btn = gr.Button("Submit", variant="primary")
 
+    with gr.Box():
+        with gr.Row():
+            axis_radio = gr.Radio(
+                choices=["Sagittal", "Coronal", "Axial"],
+                value="Axial",
+                type="index",
+                show_label=False,
+                interactive=True,
+            )
+            axis_slider = gr.Slider(
+                minimum=0,
+                maximum=159,
+                value=80,
+                step=1,
+                show_label=False,
+                interactive=True,
+            )
+        sample_img = gr.Image(
+            image_mode="L",
+            type="numpy",
+            interactive=False,
+        )
+    gr.Markdown(article)
+
+    # clear_btn.click()
+
+    submit_btn.click(
+        sample_fn,
+        [
+            seed,
+            gender_radio,
+            age_slider,
+            ventricular_slider,
+            brain_slider,
+        ],
+        sample_img,
+    )
+
+    axis_slider.change(
+        update_plotted_img,
+        axis_slider,
+        sample_img
+    )
+
+demo.launch(enable_queue=True)
