19e97c3068d3041e5b9df21b8db5270bc715ee77
Warvito <ianonimato@hotmail.com>
2022-06-11 14:45:03 +0100

    Checkpoint

--- src/start_gradio_app.py
+++ src/start_gradio_app.py
@@ -1,5 +1,30 @@
 import gradio as gr
-from sampling_functions import sample_fn, image_data
+import nibabel as nib
+import time
+
+model = None
+image_data = None
+
+def sample_fn(
+        seed,
+        gender_radio,
+        age_slider,
+        ventricular_slider,
+        brain_slider,
+):
+    print(seed)
+    print(gender_radio)
+    print(age_slider)
+    print(ventricular_slider)
+    print(brain_slider)
+    image = nib.load("/home/walter/Downloads/sub-HCD0001305_ses-01_space-MNI152NLin2009aSym_T2w.nii.gz")
+    global image_data
+    image_data = image.get_fdata()
+    time.sleep(2)
+
+    plotted_img = image_data[:, :, 80]
+    plotted_img = (plotted_img - plotted_img.min()) / (plotted_img.max() - plotted_img.min())
+    return plotted_img
 
 title = "Amigo's Brains with Diffusion Models"
 description = """
@@ -40,7 +65,11 @@ inputs_block = [
 
 def update_plotted_img(idx):
     global image_data
-    return image_data[:,:,idx]
+    print(idx)
+    plotted_img = image_data[:, :, idx]
+    plotted_img = (plotted_img - plotted_img.min()) / (plotted_img.max() - plotted_img.min())
+
+    return plotted_img
 
 
 demo = gr.Blocks()
